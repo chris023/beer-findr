521e3742ced02d7544d8db1d9f0037212ce685f0
Ashton Bailey <ashtonkbailey@gmail.com>
2018-10-23 16:21:48 -0600

    Write initial states for App, CardContainer, and Search

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './Components/Header';
+
 import Search from './Components/Search';
 import CardContainer from './Components/CardContainer';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchResults: []
+    }
+  }
+
+  setSearchResults = () => {
+
+  }
+
   render() {
     return (
       <div>
         <h1>Beer Findr</h1>
         <h2></h2>
-        <Search />
-        <CardContainer />
+        <Search setSearchResults={ this.setSearchResults } />
+        <CardContainer results={ this.state.searchResults } />
       </div>
     );
   }
