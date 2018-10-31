253a65d6100392dd04bf61b99bd1ee80285d7a17
Ashton Bailey <ashtonkbailey@gmail.com>
2018-10-30 18:12:09 -0600

    Include brewery name in profile

--- src/Components/BeerProfile.jsx
+++ src/Components/BeerProfile.jsx
@@ -7,11 +7,17 @@ class BeerProfile extends Component {
   render() {
       let profile = this.props.results.find((beer) => {
         return beer.name === this.props.beerName
+      });
+
+      let brewery = this.props.breweries.find((brewery) => {
+        return brewery.breweryID === profile.breweryID
       })
+
       return (
           <div onClick={this.props.changeView} className="beer-profile">
             <img className="beer-img" src={profile.image} alt={profile.name} />
             <p>{profile.name}</p>
+            <p>{brewery.name}</p>
             <p>{profile.abv}</p>
             <p>{profile.styles}</p>
             <p>{profile.tastingNotes}</p> 
@@ -21,4 +27,3 @@ class BeerProfile extends Component {
 }
 
 export default BeerProfile;
-            // <p>{brewery.name}</p>
